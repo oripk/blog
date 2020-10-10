d49b17d79ed75da74b7c26d8ae1d067013fcb63c
oripk <45393030+oripk@users.noreply.github.com>
2020-10-10 14:05:53 +0000

    add home document list

--- routes/home.js
+++ routes/home.js
@@ -2,16 +2,24 @@
 
 var express = require('express');
 var router = express.Router();
+var Post = require('../models/Post');
 
 // Home
 router.get('/', function(req, res){
-  res.render('home/welcome');
+  Post.find({})
+  .sort('-createdAt')
+  .exec((err, posts) => {
+    if(err) return res.json(err);
+    res.render('home/welcome', {posts:posts}) ;
+  });
 });
 
+//About
 router.get('/about', function(req, res){
   res.render('home/about');
 });
 
+//Contact
 router.get('/contact', function(req, res){
   res.render('home/contact');
 })
--- routes/posts.js
+++ routes/posts.js
@@ -5,56 +5,56 @@ var router = express.Router();
 var Post = require('../models/Post');
 
 // Index 
-router.get('/', function(req, res){
+router.get('/', (req, res) => {
   Post.find({})                  
   .sort('-createdAt')            
-  .exec(function(err, posts){    
+  .exec((err, posts) => {    
     if(err) return res.json(err);
     res.render('posts/index', {posts:posts});
   });
 });
 
 // New
-router.get('/new', function(req, res){
+router.get('/new', (req, res) => {
   res.render('posts/new');
 });
 
 // create
-router.post('/', function(req, res){
-  Post.create(req.body, function(err, post){
+router.post('/', (req, res) =>{
+  Post.create(req.body, (err, post) => {
     if(err) return res.json(err);
     res.redirect('/posts');
   });
 });
 
 // show
-router.get('/:id', function(req, res){
-  Post.findOne({_id:req.params.id}, function(err, post){
+router.get('/:id', (req, res) => {
+  Post.findOne({_id:req.params.id}, (err, post) => {
     if(err) return res.json(err);
     res.render('posts/show', {post:post});
   });
 });
 
 // edit
-router.get('/:id/edit', function(req, res){
-  Post.findOne({_id:req.params.id}, function(err, post){
+router.get('/:id/edit', (req, res) => {
+  Post.findOne({_id:req.params.id}, (err, post) => {
     if(err) return res.json(err);
     res.render('posts/edit', {post:post});
   });
 });
 
 // update
-router.put('/:id', function(req, res){
+router.put('/:id', (req, res) => {
   req.body.updatedAt = Date.now(); 
-  Post.findOneAndUpdate({_id:req.params.id}, req.body, function(err, post){
+  Post.findOneAndUpdate({_id:req.params.id}, req.body, (err, post) => {
     if(err) return res.json(err);
     res.redirect("/posts/"+req.params.id);
   });
 });
 
 // destroy
-router.delete('/:id', function(req, res){
-  Post.deleteOne({_id:req.params.id}, function(err){
+router.delete('/:id', (req, res) => {
+  Post.deleteOne({_id:req.params.id}, (err) => {
     if(err) return res.json(err);
     res.redirect('/posts');
   });
